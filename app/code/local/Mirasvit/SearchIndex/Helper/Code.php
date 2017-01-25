fc0ada90ee9d716e5dec666ef34903982ce0c7b4
Froxlor Customer <magento@dev.gastro.lan>
2017-01-25 11:16:11 +0100

    first commit

---
+++ app/code/local/Mirasvit/SearchIndex/Helper/Code.php
@@ -0,0 +1,10 @@
+<?php
+class Mirasvit_SearchIndex_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "4GACEFU13L";
+    protected $s = "SSP";
+    protected $l = "25103";
+    protected $v = "2.3.21";
+    protected $b = "977";
+    protected $d = "gastrodax.de";
+}
